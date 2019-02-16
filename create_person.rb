ee1d4006c838484de5d737c22fb77d582baa7465
Chad Logsdon <cavejohnson93@gmail.com>
2019-02-16 13:51:48 -0600

    made tictactoe

--- create_person.rb
+++ create_person.rb
@@ -1,3 +1,18 @@
+# puts "Enter First name"
+# firstnamevalue = gets.chop
+# puts "Enter Last name"
+# lastnamevalue = gets.chop
+# puts "Enter Age"
+# agevalue = gets.chop.to_i
+
+# hashage = { firstname: firstnamevalue, lastname: lastnamevalue, age: agevalue}
+
+# puts hashage.each {|key,value| puts value}
+
+def createperson (firstname,lastname,age)
+  return {firstname: firstname, lastname: lastname, age: age}
+  end
+
 puts "Enter First name"
 firstnamevalue = gets.chop
 puts "Enter Last name"
@@ -5,6 +20,6 @@ lastnamevalue = gets.chop
 puts "Enter Age"
 agevalue = gets.chop.to_i
 
-hashage = { firstname: firstnamevalue, lastname: lastnamevalue, age: agevalue}
+puts createperson(firstnamevalue, lastnamevalue, agevalue)
 
-puts hashage.each {|key,value| puts value}
+
